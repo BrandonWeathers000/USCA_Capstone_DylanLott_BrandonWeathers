69a586ec7d2e8295a9334722fe6f2b112a87f42b
BrandonWeathers000 <brandon.r.weathers@gmail.com>
2026-01-27 00:43:08 -0500

    almost done

--- MultiTesterTools/RefinedLCSM.java
+++ MultiTesterTools/RefinedLCSM.java
@@ -1,7 +1,10 @@
 package MultiTesterTools;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @version 1.0
@@ -36,92 +39,43 @@ public class RefinedLCSM{
         // input.add("Bobby2abc");
         // input.add("Bobby3abc");
 
-        List<String> results = new ArrayList<>();
-        results = returnAllCommonSubstrings(input);
+        Set<String> results = new HashSet<>();
+        results = maximalCommonSubstrings("Bobby1abc", "Bobby2abc");
         results.forEach((currentPassword) -> System.out.print(currentPassword + ", "));
     }
 
-    /**
-     * This method returns all of the LONGEST common substrings given an ArrayList of input strings.
-     *
-     * @param allInputs the ArrayList of all String inputs
-     * @return a List of Strings that represent all common substring withtin all inputs
-     */
-    public static List<String> returnAllCommonSubstrings(ArrayList<String> allInputs){
-        List<String> results = new ArrayList<>();
-        results = returnCommonSubstrings(allInputs.get(0), allInputs.get(1));
-        if(results .size() == 0){
-            System.out.println("No common substrings.");
-            return results;
-        }
-        // System.out.println("The substrings from S1 and S2 are:");
-        // results.forEach((currentSubstring) -> System.out.println("\"" + currentSubstring + "\""));
-        // System.out.println();
-        for(int index = 2; index < allInputs.size(); index++){
-            singleAuxiliaryComparison(allInputs.get(index), results);
-        }
-
-        return results;
+    public static returnAllSubstringMultiInput(ArrayList<String> input){;
+        ArrayList<String> results = new ArrayList<Srings>();
     }
 
-    /**
-     * This method returns all the common substrings given just two input Strings
-     *
-     * @param s1 the first string to be compared
-     * @param s2 the second string to be compared
-     * @return result a List of Strings the both inputs share
-     */
-    public static List<String> returnCommonSubstrings(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+    public static Set<String> maximalCommonSubstrings(String s, String t){
+        int[][] table = new int[s.length()][t.length()];
+        Set<String> result = new HashSet<>();
 
-        int[][] LCSuf = new int[m + 1][n + 1];
-        int maxLen = 0;
-        List<String> result = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++){
+            for (int j = 0; j < t.length(); j++){
+                if (s.charAt(i) != t.charAt(j)){
+                    continue;
+                }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    LCSuf[i][j] = LCSuf[i - 1][j - 1] + 1;
-                    // res = Math.max(res, LCSuf[i][j]);
+                table[i][j] = (i == 0 || j == 0) ? 1 : table[i - 1][j - 1] + 1;
 
-                    if(LCSuf[i][j] > maxLen){
-                        maxLen = LCSuf[i][j];
-                        result.clear();
-                        result.add(s1.substring(i - maxLen, i));
-                    }else if(LCSuf[i][j] == maxLen && maxLen > 0){
-                        result.add(s1.substring(i - maxLen, i));
-                    }
+                int len = table[i][j];
+
+                boolean cannotExtend = (i == s.length() - 1) || (j == t.length() - 1) || (s.charAt(i + 1) != t.charAt(j + 1));
+
+                if (len > 0 && cannotExtend){
+                    result.add(s.substring(i - len + 1, i + 1));
                 }
             }
         }
+        // for(int[] row : table){
+        //     for(int cell : row){
+        //         System.out.print(cell + " ");
+        //     }
+        //     System.out.println();
+        // }
+
         return result;
-    }
-
-    /**
-     * This method compares the single other input string to the list of possible results to see if they match
-     *
-     * @param otherInputString the 3rd through nth element of all inputs to be compared
-     * @param results the list to be checked against
-     * @return a List of String that represent all elements that pass the conditions
-     */
-    public static List<String> singleAuxiliaryComparison(String otherInputString, List<String> results){
-        for(int index = 0; index <= results.size(); index++){
-            List<String> possibleNewSubstrings = new ArrayList<>();
-            possibleNewSubstrings = new ArrayList<>();
-            possibleNewSubstrings = returnCommonSubstrings(otherInputString, results.get(0));
-            // System.out.println("The comparison of " + otherInputString + " and " + results.get(0) + " leaves the results looking as such:");
-            if(!(possibleNewSubstrings.isEmpty())){
-                for(String currentPossibleString : possibleNewSubstrings){
-                    results.add(currentPossibleString);
-                    results.remove(0);
-                }
-            }else{
-                results.remove(0);
-            }
-            // results.forEach((currentSubstring) -> System.out.println("\"" + currentSubstring + "\""));
-            // System.out.println();
-        }
-        return results;
     }
 }
