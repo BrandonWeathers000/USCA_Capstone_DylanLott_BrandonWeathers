caa477ba29675f1311f1439a70c07b9caacdcf7b
Brandon Weathers <brandon@debian.debian>
2026-02-18 16:01:12 -0500

    finished basic graphic functions

--- EPSB/docs/type-search-index.js
+++ EPSB/docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"EPSB"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"EPSB"},{"p":"<Unnamed>","l":"Little_EPSB_CLI_Testing"}];updateSearchResults();
